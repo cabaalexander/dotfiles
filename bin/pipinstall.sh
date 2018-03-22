bc5c53bbf002912b73437bea090b1d40971d3f3b
cabaalexander <cabaalexander321@gmail.com>
2018-03-22 14:11:36 -0400

    Refactor utils * Split install functions into its own file * Make .sh files executable

---
+++ bin/pipinstall.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+. ./bin/utils.sh
+
+pipInstall(){
+  # Install modules for python
+  [ -n "$1" ] \
+    && MODULE=$1 \
+    || die "What module do you want to install?"
+  echo "[Installing Python] ${MODULE}"
+  pip3 install ${MODULE} &> /dev/null
+}
+
+
+# If this file is running in terminal call the function `pipInstall`
+# Otherwise just source it
+if [ "$(basename ${0})" = "pipinstall.sh" ]
+then
+  pipInstall "${@}"
+fi
+
+
--- nvim/snippets/bin-function.sh
+++ nvim/snippets/bin-function.sh
@@ -4,6 +4,7 @@ functionName(){
 }
 
 # If this file is running in terminal call the function `functionName`
+# Otherwise just source it
 if [ "$(basename ${0})" = "scriptname.sh" ]
 then
   functionName "${@}"
