bfb19d934eaf0b4e9043424476c7aa7d4596f9b2
cabaalexander <cabaalexander321@gmail.com>
2018-03-22 14:11:36 -0400

    Refactor utils * Split install functions into its own file * Make .sh files executable

---
+++ bin/updateapt.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+updateAPT(){
+  # Update APT repositories
+  echo "[Updating APT]"
+  sudo apt-get update &> /dev/null
+}
+
+
+# If this file is running in terminal call the function `updateAPT`
+# Otherwise just source it
+if [ "$(basename ${0})" = "updateapt.sh" ]
+then
+  updateAPT "${@}"
+fi
+
+
--- bin/utils.sh
+++ bin/utils.sh
@@ -27,35 +27,7 @@ function echo_yellow(){
   echo -e "${YELLOW}$@${NC}"
 }
 
-# Install gems for ruby
-gemInstall(){
-  GEM=$1
-  echo "[Installing Gem] ${GEM}"
-  gem install ${GEM} &> /dev/null
-}
-
-# Install modules for python
-pipInstall(){
-  MODULE=$1
-  echo "[Installing Python] ${MODULE}"
-  pip3 install ${MODULE} &> /dev/null
-}
-
-# This is to install apps without showing a bunch stuffs on the stdout
-installApp(){
-  local APP=$1
-
-  echo "[Installing] ${APP}"
-  apt-get install -y ${APP} &> /dev/null
-}
-
-# Update APT repositories
-updateAPT(){
-  echo "[Updating APT]"
-  apt-get update &> /dev/null
-}
-
-# If this file is running in terminal call the function `normalizePath`
+# If runing in terminal execute one of the functions if not just source it
 if [ "$(basename ${0})" = "utils.sh" ]
 then
   UTIL=${1}
@@ -72,18 +44,6 @@ then
       ;;
     "echo_yellow")
       echo_yellow "${@}"
-      ;;
-    "gemInstall")
-      gemInstall "${@}"
-      ;;
-    "installApp")
-      installApp "${@}"
-      ;;
-    "updateAPT")
-      updateAPT "${@}"
-      ;;
-    "pipInstall")
-      pipInstall "${@}"
       ;;
   esac
 fi
