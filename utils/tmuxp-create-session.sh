25997cf45df78f375095ca2dbf18d7d7eefd9bc9
cabaalexander <cabaalexander321@gmail.com>
2018-05-22 22:15:49 -0400

    Refactor `tmuxp-create-session.sh`
    [home]

--- utils/tmuxp-create-session.sh
+++ utils/tmuxp-create-session.sh
@@ -2,17 +2,21 @@
 
 TMUX_SESSION_PATH="/tmp/tmuxp.yaml"
 
-read -p "Session Name (default-ss): " SESSION_NAME
-read -p "Window Name (Code): " WINDOW_NAME
-read -p "CMD (echo): " CMD
+DEFAULT_SESSION_NAME="default-ss"
+DEFAULT_WINDOW_NAME="asdf"
+DEFAULT_COMMAND="cd"
+
+read -p "Session Name (${DEFAULT_SESSION_NAME}): " SESSION_NAME
+read -p "Window Name (${DEFAULT_WINDOW_NAME}): " WINDOW_NAME
+read -p "CMD (${DEFAULT_COMMAND}): " CMD
 
 cat << EOF > ${TMUX_SESSION_PATH}
-session_name: ${SESSION_NAME:-default-ss}
+session_name: ${SESSION_NAME:-${DEFAULT_SESSION_NAME}}
 windows:
-- window_name: ${WINDOW_NAME:-Code}
+- window_name: ${WINDOW_NAME:-${DEFAULT_WINDOW_NAME}}
   layout: tiled
   shell_command_before:
-    - ${CMD:-echo}
+    - ${CMD:-${DEFAULT_COMMAND}}
   panes:
     - shell_command:
       - clear
