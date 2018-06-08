f202363e7e76b434f58c420ee630765bda6bda83
cabaalexander <cabaalexander321@gmail.com>
2018-06-08 15:22:38 -0400

    Refactor ruby script

--- config/install/rvm.sh
+++ config/install/rvm.sh
@@ -4,8 +4,7 @@ rvmInstall(){
   # Ruby
   echo "[Installing] Ruby ${RUBY}"
   (
-  gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-  \curl -sSL https://get.rvm.io | bash -s stable --ruby=${RUBY}
+  curl -sSL https://get.rvm.io | bash -s stable --ruby=${RUBY}
   ) &> /dev/null
   source ${HOME}/.rvm/scripts/rvm
 }
