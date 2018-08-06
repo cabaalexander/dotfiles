5ff5e34f5c790382a8de687ce9aea78b2c33f15d
cabaalexander <cabaalexander321@gmail.com>
2018-08-06 09:26:09 -0400

    :hammer: Create script for installing `htop-vim`
    [notebook]

---
+++ config/install/htop-vim.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+htopVIMInstall(){
+  TEMP_HTOP_PATH="/tmp/htop-vim"
+
+  rm -rf ${TEMP_HTOP_PATH}
+
+  git clone https://github.com/KoffeinFlummi/htop-vim.git ${TEMP_HTOP_PATH}
+
+  cd ${TEMP_HTOP_PATH}
+
+  sh autogen.sh
+
+  ./configure --disable-unicode && make
+
+  sudo make install
+
+  cd -
+
+  rm -rf ${TEMP_HTOP_PATH}
+}
+
+# If this file is running in terminal call the function `htopVIMInstall`
+# Otherwise just source it
+if [ "$(basename ${0})" = "htop-vim.sh" ]
+then
+  htopVIMInstall "${@}"
+fi
+
+
