0fd8c04af5dfc8288a711b77b3cccabe92aba38f
cabaalexander <cabaalexander321@gmail.com>
2018-09-14 00:00:48 -0400

    :wrench: Refactor `~/.bash/autocomplete` * Rename `extractcaseoption` -> `getcaseoptions`
    [notebook]

--- dots/.bin/extractcaseoption.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-extractCaseOption(){
-  local FILE=$1
-  echo $(grep "^[ ]\+\"\?[-_a-zA-Z0-9]\+\"\?)$" ${FILE} | sed 's:[") ]::g')
-}
-
-# `basename` throws an error if I try to use here ¯\_(ツ)_/¯
-
