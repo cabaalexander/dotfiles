625d0beb7b6454caa68eb0aaf7ae2f4d004de0ee
cabaalexander <cabaalexander321@gmail.com>
2018-10-03 00:01:17 -0400

    :hammer: Add `dowhile` binary
    [notebook]

---
+++ dots/.bin/dowhile.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+TIME_RE="^[0-9]+[smh]?$"
+
+if [[ "$1" == "-t" ]]
+then
+  [[ $2 =~ $TIME_RE ]] || {
+    echo "Option (-t) valid format: [0-9]+s|m|h"
+    exit 1
+  }
+  TIME=$2
+  shift 2
+fi
+
+while :
+do
+  $@
+  sleep ${TIME:-1}
+done
+
