3150c3d62f31c891e9f1107ba21a71aadbb4abc5
cabaalexander <cabaalexander321@gmail.com>
2018-11-02 13:55:40 -0400

    :hammer: Clean constants * Refactor ruby install script * Set strict mode on install * Update NVIM

--- config/install/rvm.sh
+++ config/install/rvm.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 rvmInstall(){
-  local RUBY="2.2"
-
-  gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-  \curl -sSL https://get.rvm.io | bash -s stable --ruby="${RUBY}"
+  bash -c "$(curl -s get.rvm.io)" --ruby=2.2
 }
 
 # If this file is running in terminal call the function `rvmInstall`
