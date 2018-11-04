9e01a2040174ae8ae4b0069e8ad634f86957c4a3
cabaalexander <cabaalexander321@gmail.com>
2018-11-04 11:42:47 -0400

    :hammer: Make all the managers executable via `CLI`

--- config/managers/gem.sh
+++ config/managers/gem.sh
@@ -6,3 +6,10 @@ __gem(){
   gem install "$@"
 }
 
+# If this file is running in terminal call the function `__gem`
+# Otherwise just source it
+if [ "$(basename "${0}")" = "gem.sh" ]
+then
+  __gem "${@}"
+fi
+
