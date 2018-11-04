01cf98516036da73ff8cd3d1cd2822048487dc8b
cabaalexander <cabaalexander321@gmail.com>
2018-11-04 11:42:47 -0400

    :hammer: Make all the managers executable via `CLI`

--- config/managers/node.sh
+++ config/managers/node.sh
@@ -4,3 +4,10 @@ __node(){
   yarn global add "$@"
 }
 
+# If this file is running in terminal call the function `__node`
+# Otherwise just source it
+if [ "$(basename "${0}")" = "node.sh" ]
+then
+  __node "${@}"
+fi
+
