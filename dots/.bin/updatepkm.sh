44cd52d2f26cb99ddc480bc7403332b1f1f181be
cabaalexander <cabaalexander321@gmail.com>
2018-11-28 11:31:09 -0400

    Fix `*.sh` errors * Remove old scripts

--- dots/.bin/updatepkm.sh
+++ dots/.bin/updatepkm.sh
@@ -7,7 +7,7 @@ updatePKM(){
 
 # If this file is running in terminal call the function `updatePKM`
 # Otherwise just source it
-if [ "$(basename ${0})" = "updatepkm.sh" ]
+if [ "$(basename "${0}")" = "updatepkm.sh" ]
 then
   updatePKM "${@}"
 fi
