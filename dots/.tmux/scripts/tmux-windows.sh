44bb9e1e638c82edc3ad765b74df0ad48fd84e56
cabaalexander <cabaalexander321@gmail.com>
2018-12-26 16:47:42 -0400

    feat: 🎸 Add `diff-so-fancy`
    Add needed `git/config` and indent it

--- dots/.tmux/scripts/tmux-windows.sh
+++ dots/.tmux/scripts/tmux-windows.sh
@@ -3,7 +3,7 @@
 # "Swap window - New window index:"
 tmux list-windows \
   | cut -d' ' -f1-2 \
-  | sed -r 's/:|[*-]$//g' \
+  | sed -E 's/:|[*-]$//g' \
   | tr '\n' ' ' \
-  | sed -r 's/[ ]+$//'
+  | sed -E 's/[ ]+$//'
 
