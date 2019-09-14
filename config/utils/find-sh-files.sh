6907d7cc6412a4978fef45cc0b91adbf0b06c27b
cabaalexander <cabaalexander321@gmail.com>
2019-09-14 00:26:43 -0400

    fix: 🐛 Ignore `Session.vim` files from `utils/find-sh-files`
    This avoid openning tha annoying Session file when you are prompted to
    fix the *.sh files

--- config/utils/find-sh-files.sh
+++ config/utils/find-sh-files.sh
@@ -5,24 +5,21 @@ DIR_ARR=(
     dots/.zsh
 )
 
-__find_sh_files(){
-    local DIR_PATH
+__find_sh_files() {
+    local DIR_PATH OPTIONS
+    OPTIONS='! -path "*.git/*" ! -name "Session.vim"'
 
-    find . -name "*.sh" -type f ! -path "*.git/*"
+    eval "find . -name '*.sh' -type f $OPTIONS"
 
-    for DIR_PATH in ${DIR_ARR[*]}
-    do
-        if [ -d "$DIR_PATH" ]
-        then
-            find "$DIR_PATH" -type f
+    for DIR_PATH in ${DIR_ARR[*]}; do
+        if [ -d "$DIR_PATH" ]; then
+            eval "find '$DIR_PATH' -type f $OPTIONS"
         fi
     done
 }
 
 # If this file is running in terminal call the function `__find_sh_files`
 # Otherwise just source it
-if [ "$(basename "${0}")" = "find-sh-files.sh" ]
-then
-  __find_sh_files "${@}"
+if [ "$(basename "${0}")" = "find-sh-files.sh" ]; then
+    __find_sh_files "${@}"
 fi
-
