00225c4f9021745301b23dc07952133e7fce449f
cabaalexander <cabaalexander321@gmail.com>
2019-09-14 01:32:03 -0400

    refactor: 💡 Cleanup on `utils/update-repo.sh`

--- config/utils/update-repo.sh
+++ config/utils/update-repo.sh
@@ -1,18 +1,17 @@
 #!/bin/bash -e
 
-main(){
+main() {
     local repo repo_local_path
 
-    repo=${1:?Repo?, Provide a repo to clone}
-    repo_local_path=${2:?Need a loal path to clone the repo to}
+    repo=${1:?"Provide a repo URL to clone..."}
+    repo_local_path=${2:?"What is the path you want to clone the repo to?"}
 
-    if ! [ -e "$repo_local_path" ]; then
-        git clone "$repo" "$repo_local_path"
+    if [ -e "$repo_local_path" ]; then
+        return 0
     fi
 
-    if ! grep -q "$repo_local_path" .gitignore; then
-        echo "$repo_local_path" >> .gitignore
-    fi
+    git clone "$repo" "$repo_local_path"
+    echo "$repo_local_path" >>.gitignore
 }
 
 main "$@"
