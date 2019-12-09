787586a57053a3f1499779d5a179b546918284ee
cabaalexander <cabaalexander321@gmail.com>
2019-12-09 17:37:06 -0400

    refactor: 💡 Better `setzsh`, add some mac dependencies

--- config/functions/setzsh.sh
+++ config/functions/setzsh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 setzshInstall() {
-    local IS_ZSH
+    local IS_ZSH PASS
 
     IS_ZSH=$(
         if [ "$(basename "$SHELL")" == "zsh" ]; then
@@ -10,8 +10,19 @@ setzshInstall() {
     )
 
     # set zsh as default shell
-    if [ -n "$IS_ZSH" ] || ! command -v zsh &>/dev/null; then
+    if [ -n "$IS_ZSH" ]; then
+        echo "You have zsh already set"
         return 0
+    elif ! command -v zsh &>/dev/null; then
+        echo "You do not have zsh installet m8..."
+        return 0
+    fi
+
+    # set zsh as default shell
+    if ! [ -f "$G_PASSWORD" ]; then
+        G_PASSWORD=$(mktemp)
+        read -rsp "Sudo password: " PASS
+        echo "$PASS" > "$G_PASSWORD"
     fi
 
     chsh -s "$(grep /zsh$ /etc/shells | tail -1)" <"$G_PASSWORD"
